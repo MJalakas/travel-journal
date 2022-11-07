6edb79b2eaa461a59b65b9179209329bda500485
nuf1k <marten@jalakas.ee>
2022-11-07 22:57:28 +0200

    Card.js now takes in props from data.js. mvp ready.

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,19 +15,17 @@ export default function Card(props) {
             <div className="card--location-logo-wrapper">
               <img className="card--location-logo" src={locLogo} />
             </div>
-            <div className="card--location-text">JAPAN</div>
-            <a className="card--location-gmaps" href="">
+            <div className="card--location-text">{props.location}</div>
+            <a className="card--location-gmaps" href={props.googleMapsUrl}>
               View on Google Maps
             </a>
           </div>
 
-          <div className="card--title">Mount Fuji</div>
-          <div className="card--date">12 Jan, 2021 - 24 Jan, 2021</div>
-          <div className="card--description">
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776
-            meters (12,380 feet). Mount Fuji is the single most popular tourist
-            site in Japan, for both Japanese and foreign tourists.
+          <div className="card--title">{props.title}</div>
+          <div className="card--date">
+            {props.startDate} - {props.endDate}
           </div>
+          <div className="card--description">{props.description}</div>
         </div>
       </div>
       <hr className="card-separator"></hr>
--- src/data.js
+++ src/data.js
@@ -1,5 +1,16 @@
 export default [
   {
+    title: "Amsterdam",
+    location: "Netherlands",
+    googleMapsUrl: "https://goo.gl/maps/m6b9xUgaFpqYZ2Mw9",
+    startDate: "1 July, 2021",
+    endDate: "24 July, 2021",
+    description:
+      "Amsterdam is the Netherlands’ capital, known for its artistic heritage, elaborate canal system and narrow houses with gabled facades, legacies of the city’s 17th-century Golden Age.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1534351590666-13e3e96b5017?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  },
+  {
     title: "Mount Fuji",
     location: "Japan",
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
@@ -10,23 +21,14 @@ export default [
     imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
   },
   {
-    title: "Mount asd",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    title: "Antarctica",
+    location: "Somewhere very southern",
+    googleMapsUrl: "https://goo.gl/maps/6AUbKrj9JbYKtE4N6",
+    startDate: "18 Sep, 2020",
+    endDate: "26 Oct, 2020",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
-  },
-  {
-    title: "Mount adga",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
-    description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+      "Antarctica, the southernmost continent and site of the South Pole, is a virtually uninhabited, ice-covered landmass. Most cruises to the continent visit the Antarctic Peninsula, which stretches toward South America.",
+    imageUrl:
+      "https://cdn.britannica.com/08/135708-050-2346C1CF/Paradise-Bay-Antarctica.jpg",
   },
 ];
